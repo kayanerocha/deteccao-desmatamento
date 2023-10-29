c9867503d8e55f9c0aa162bac50ef180b5336246
Kayane Rocha <kayannerch@gmail.com>
2023-10-29 18:41:20 -0300

    ajusanto caminhos para imagens

--- aprendizado_profundo.py
+++ aprendizado_profundo.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import glob, os
 import re
 from PIL import Image
+import keras
+import tensorflow as tf
 
 def pre_processamento(diretorio, tamanho_maximo):
     # Converte as imagens em matrizes e escala de cinza
@@ -35,11 +37,12 @@ def conjunto_dados(diretorio, tamanho_maximo):
 
         return (np.asarray(imagens), np.asarray(nomes))
 
+diretorio_atual = os.getcwd()
 tamanho_maximo = 130, 130
 # Conjunto de dados para treinamento
-(imagens_treinamento, nomes_treinamento) = conjunto_dados('./img/conjunto_dados/treinamento', tamanho_maximo)
+(imagens_treinamento, nomes_treinamento) = conjunto_dados(f'{diretorio_atual}\\img\\conjunto_dados\\treinamento', tamanho_maximo)
 # Conjunto de dados para testes
-(imagens_testes, nomes_testes) = conjunto_dados('C:/Users/OI416936/github/deteccao-desmatamento/img/conjunto_dados/testes', tamanho_maximo)
+(imagens_testes, nomes_testes) = conjunto_dados(f'{diretorio_atual}\\img\\conjunto_dados\\testes', tamanho_maximo)
 
 nomes = ['desmatamento', 'floresta']
 
@@ -52,3 +55,48 @@ print(nomes_treinamento)
 imagens_testes.shape
 (6, 130, 130)
 print(nomes_testes)
+
+# Classifica as imagens em valores entre 0 e 1
+imagens_treinamento = imagens_treinamento / 255.0
+imagens_testes = imagens_testes / 255.0
+
+# Configura as camadas do modelo
+modelo = keras.Sequential([
+    keras.layers.Flatten(input_shape=(130, 130)), # Achata as imagens em um arranjo
+    keras.layers.Dense(128, activation=tf.nn.sigmoid),
+    keras.layers.Dense(16, activation=tf.nn.sigmoid),
+    keras.layers.Dense(2, activation=tf.nn.softmax)
+])
+
+# Compila o modelo
+sgd = keras.optimizers.SGD(lr=0.01, momentum=0.7, nesterov=True)
+modelo.compile(optimizer=sgd,
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+# Treina o modelo
+modelo.fit(imagens_treinamento, nomes_treinamento, epochs=100)
+
+# Avalia a precisão
+precisao_teste, acuracia_teste = modelo.evaluate(imagens_testes, nomes_testes)
+print('Acurácia teste:', acuracia_teste)
+
+# Previsões
+previsoes = modelo.predict(imagens_testes)
+print(previsoes)
+
+def resultado(imagens, tipos):
+        plt.figure(figsize=(10,10))
+        tamanho = min(25, len(imagens))
+
+        for i in range(tamanho):
+            plt.subplot(5, 5, i+1)
+            plt.xticks([])
+            plt.yticks([])
+            plt.grid(False)
+            plt.imshow(imagens[i], cmap=plt.cm.binary)
+            plt.xlabel(nomes[tipos[i]])
+
+# Mostra o resultado
+resultado(imagens_testes, np.argmax(previsoes, axis = 1))
+plt.show()
